f12e05f3e4132b6c7597cd1bd1008bae8b576662
adi-kgp <adi.kgp@gmail.com>
2020-01-28 15:27:51 +0530

    updating our app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import {Switch,Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
@@ -15,7 +15,7 @@ class App extends React.Component {
 
   componentDidMount(){  
     const {setCurrentUser} = this.props;
-    
+
     this.unSubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
       if (userAuth){
         const userRef = await createUserProfileDocument(userAuth);
@@ -43,7 +43,8 @@ class App extends React.Component {
         <Switch>
         <Route exact path = '/' component= {HomePage} />
         <Route path = '/shop' component= {ShopPage} />
-        <Route path = '/signin' component= {SignInAndSignUp} />
+        <Route exact path = '/signin' 
+          render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUp />)} />
         </Switch>
       </div>
     );  
@@ -51,8 +52,12 @@ class App extends React.Component {
   
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
