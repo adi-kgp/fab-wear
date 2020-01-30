d9b03633a70867c5610ee0deabaade5d503aae2f
adi-kgp <adi.kgp@gmail.com>
2020-01-30 20:03:16 +0530

    created checkout item component and page, and updated cart to use selectors

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import {Switch, Route, Redirect} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckOutPage from './pages/checkout/checkout.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import {connect} from 'react-redux';
 import {setCurrentUser} from './redux/user/user.actions';
+import {selectCurrentUser} from './redux/user/user.selector';
+import {createStructuredSelector} from 'reselect';
 
 class App extends React.Component {
 
@@ -43,6 +46,7 @@ class App extends React.Component {
         <Switch>
         <Route exact path = '/' component= {HomePage} />
         <Route path = '/shop' component= {ShopPage} />
+        <Route exact path = '/checkout' component= {CheckOutPage} />
         <Route exact path = '/signin' 
           render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUp />)} />
         </Switch>
@@ -52,9 +56,9 @@ class App extends React.Component {
   
 }
 
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
-})
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+});
 // 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -4,8 +4,7 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   if (existingCartItem){
     return cartItems.map(cartItem => 
       cartItem.id === cartItemToAdd.id ?
-        {...cartItem, quantity: cartItem.quantity + 1} : cartItem
-      )
+        {...cartItem, quantity: cartItem.quantity + 1} : cartItem)
   }
   return [...cartItems, {...cartItemToAdd, quantity: 1}]
 }
