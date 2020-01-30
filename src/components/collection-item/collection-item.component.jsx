3660cba771f9c14c50591b7b7798496997aa632f
adi-kgp <adi.kgp@gmail.com>
2020-01-30 22:55:16 +0530

    added addItem and removeItem functionality to checkout item component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './collection-item.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
 import {connect} from 'react-redux';
-import addItem  from '../../redux/cart/cart.actions';
+import {addItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({item, addItem}) => {
+const CollectionItem = ({item, addItemToCollection}) => {
   const {name, price, imageUrl} = item;
   return (
   <div className='collection-item'>
@@ -17,13 +17,13 @@ const CollectionItem = ({item, addItem}) => {
         <span className='name'> {name} </span>
         <span className='price'> {price} </span>
       </div>
-    <CustomButton onClick = {() => addItem(item)} inverted >Add to Cart </CustomButton>
+    <CustomButton onClick = {() => addItemToCollection(item)} inverted >Add to Cart</CustomButton>
   </div>
-  )
+  );
 }
 
 const mapDispatchToProps = dispatch => ({
-  addItem: item => dispatch(addItem(item))
+  addItemToCollection: item => dispatch(addItem(item))
 })
 
 
